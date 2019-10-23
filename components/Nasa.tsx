2b0be52f141da1fb050d7fcff4708119bfc2e0c9
JanezSedeljsak <43420276+JanezSedeljsak@users.noreply.github.com>
2019-10-23 21:53:29 +0200

    nasa component temporary fix

--- components/Nasa.tsx
+++ components/Nasa.tsx
@@ -17,27 +17,35 @@ export default class App extends React.Component<Props, State> {
         super(props);
         this.state = {
             isLoading: false,
-            data: null
+            data: { 
+                "date":"2019-10-23",
+                "explanation":"The painting Starry Night is one of the most famous icons of the night sky ever created.   The scene was painted by Vincent van Gogh in southern France in 1889.   The swirling style of Starry Night appears, to many, to make the night sky come alive.   Although van Gogh frequently portrayed real settings in his paintings, art historians do not agree on precisely what stars and planets are being depicted in Starry Night.   The style of Starry Night is post-impressionism, a popular painting style at the end of the nineteenth century.  The original Starry Night painting hangs in the Museum of Modern Art in New York City, New York, USA.   New: APOD Read to You by AI",
+                "hdurl":"https://apod.nasa.gov/apod/image/1910/StarryNight_VanGogh_30000.jpg",
+                "media_type":"image",
+                "service_version":"v1",
+                "title":"Starry Night by Vincent van Gogh",
+                "url":"https://apod.nasa.gov/apod/image/1910/StarryNight_VanGogh_970.jpg"
+            }
         };
     }
 
     componentWillMount() {
-        fetch(
+        /*fetch(
             'https://api.nasa.gov/planetary/apod?api_key=bzYdhe9wEL2NXaCklkQWwoMMXVBeouzkTKA0qYXA'
         )
             .then(x => x.json())
-            .then(result => this.setState({ data: result }));
+            .then(result => this.setState({ data: result }));*/
     }
 
     render() {
-        const { data } = this.state;
+        const { data } = this.state
         return (
             <View>
-                <Text>{data.title}</Text>
                 {typeof data === 'object' &&
                     <Card key={'_dashCard'} image={{ uri: data.url }} title={data.title}>
-                        <Text>{data.explenation}</Text>
+                        
                         <Text>{data.date}</Text>
+                        <Text>{"\n"}{data.explanation}</Text>
                         <Button
                             icon={<Icon name='code' color='#ffffff' />}
                             buttonStyle={{
@@ -51,7 +59,6 @@ export default class App extends React.Component<Props, State> {
                         />
                     </Card>
                 }
-                <Button onPress={this.props.back} title='back' />
             </View>
         );
     }
