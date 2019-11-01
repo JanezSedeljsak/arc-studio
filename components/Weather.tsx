e58253ac848ffbf83f14235b27dbc6f89110dbdd
JanezTheProgramer <43420276+JanezTheProgramer@users.noreply.github.com>
2019-11-01 21:31:53 +0100

    Weather (visual update && optimizations)

--- components/Weather.tsx
+++ components/Weather.tsx
@@ -6,11 +6,11 @@ import {
     Text,
     View,
     ImageBackground,
-    FlatList
+    FlatList,
+    ScrollView
 } from 'react-native'
 import { Card, ListItem, Button, Icon, SearchBar } from 'react-native-elements'
 import moment from 'moment'
-import ForecastCard from './ForecastCard';
 
 export interface Props {
     back: any
@@ -51,6 +51,7 @@ export default class Weather extends React.Component<Props, State> {
                         longitude: position.coords.longitude
                     }), () => { this.getWeather(); }
                 );
+                console.log(this.state.forecast)
             },
             (error) => this.setState({ forecast: error.message }),
             { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
@@ -60,7 +61,7 @@ export default class Weather extends React.Component<Props, State> {
     getWeather() {
 
         // Construct the API url to call
-        let url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + this.state.latitude + '&lon=' + this.state.longitude + '&units=metric&appid=' + "70ef7d1ecc959f4ef1a91a8a4ab7a914";
+        let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${this.state.latitude}&lon=${this.state.longitude}&units=metric&appid=` + "70ef7d1ecc959f4ef1a91a8a4ab7a914";
 
         // Call the API, and set the state of the weather forecast
         fetch(url)
@@ -73,8 +74,33 @@ export default class Weather extends React.Component<Props, State> {
     }
 
     render() {
+        const { forecast } = this.state
         return (
-            <FlatList data={this.state.forecast.list} style={{ marginTop: 20 }} keyExtractor={(item : any) => item.dt_txt} renderItem={({ item }) => <ForecastCard detail={item} location={this.state.forecast.city.name} />} />
-        );
+            <ScrollView style={{ marginTop: 20, marginBottom: 50, padding: 20 }}>
+                {forecast.list && <View>
+                        {forecast.list.map((item : any, _index: String ) =>
+                            <Card
+                                key={_index}
+                                image={{ uri: `https://openweathermap.org/img/w/${item.weather[0].icon}.png` }}
+                                title={`${moment(new Date(item.dt * 1000)).format("HH:mm")}`}>
+                                <Text style={{ marginBottom: 10 }}>
+                                    {"Weather: "}
+                                    <Text style={{ fontWeight: 'bold' }}>{item.weather[0].description}{"\n"}</Text>
+                                </Text>
+                                <Text style={{ marginBottom: 10 }}>
+                                    {"Temperature: "}
+                                    <Text style={{ fontWeight: 'bold' }}>{Math.round(item.main.temp * 10) / 10}&#8451;{"\n"}</Text>
+                                </Text>
+                                <Text style={{ marginBottom: 10 }}>
+                                    {"Humidity: "}
+                                    <Text style={{ fontWeight: 'bold' }}>{item.main.humidity}{"%\n"}</Text>
+                                </Text>
+                                <Text numberOfLines={1}>{"_________________________________________________________________________________"}</Text>
+                            </Card>
+                        )}
+                    </View>
+                }
+            </ScrollView>
+        )
     }
 }
